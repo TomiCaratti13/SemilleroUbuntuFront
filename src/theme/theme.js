839d7758818d24c3dd600a126a5c43d38c9ccdc2
Tomi Caratti <tomircaratti@gmail.com>
2024-02-28 23:34:45 -0300

    initial commit to tomi branch

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,53 +2,29 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#171616',
+    azul: {
+      main: '#093C59',
     },
-    secondary: {
-      main: '#c2c2c2',
+    verde: {
+      main: '#226516',
     },
-    verdes: {
-      dark: '#366C00',
-      medium: '#70D20F',
-      light: '#99FF33'
+    blanco: {
+      main: '#fdfdfd'
     },
-    naranjas: {
-      dark: '#DD7200',
-      medium: '#F8952C',
-      light: '#FDAD58'
+    negro: {
+      main: '#090909'
     },
-  },
-  typography: {
-    titulos: {
-      fontSize: '1.75rem',
-      fontWeight: 700,
-      // ... otros estilos que quieras agregar
+    gris: {
+      claro: '#eaeaea',
+      medio: '#d2d2d2',
+      oscuro: '#6e6f70'
     },
-    subtitulos: {
-      fontSize: '1.25rem',
-      fontWeight: 500,
-      // ... otros estilos que quieras agregar
-    },
-    parrafos: {
-      fontSize: '0.75rem',
-      fontWeight: 300,
-      // ... otros estilos que quieras agregar
-    },
-    parrafosColor: {
-      fontSize: '0.75rem',
-      fontWeight: 300,
-      backgroundColor: '#C2C2C2'
-      // ... otros estilos que quieras agregar
+    gestion: {
+      exito: '#1d9129',
+      nogestionada: '#b86b11',
+      error: '#bc1111'
     },
   },
-  shape: {
-    borderRadius: 4,  // Valor por defecto
-    borderRadiusSmall: 2,
-    borderRadiusMedium: 8,
-    borderRadiusLarge: 16,
-  },
-  // Podes agregar más configuraciones del tema aca cuando necesites
 });
 
 export default theme;
