c623bc9fc865f85f9032de0f13310bc46c959e9f
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-02-28 23:54:40 -0300

    Feat Cards and Slider cards

---
+++ src/components/SliderCard.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from 'react';
+import '../assets/styles/sliderCard.css';
+import CardMedia from '@mui/material/CardMedia';
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+export const SliderCard = ({ imgs }) => {
+	const listRef = useRef();
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	useEffect(() => {
+		const listNode = listRef.current;
+		const imgNode = listNode.querySelectorAll('li > img')[currentIndex];
+
+		if (imgNode) {
+			imgNode.scrollIntoView({
+				behavior: 'smooth',
+			});
+		}
+	}, [currentIndex]);
+
+	const scrollToImage = (direction) => {
+		if (direction === 'prev') {
+			setCurrentIndex((curr) => {
+				const isFirstSlide = currentIndex === 0;
+				return isFirstSlide ? 0 : curr - 1;
+			});
+		} else {
+			const isLastSlide = currentIndex === imgs.length - 1;
+			if (!isLastSlide) {
+				setCurrentIndex((curr) => curr + 1);
+			}
+		}
+	};
+
+	const goToSlide = (slideIndex) => {
+		setCurrentIndex(slideIndex);
+	};
+	return (
+		<CardMedia
+			className="slider-container"
+			sx={{
+				margin: 'auto',
+				objectFit: 'cover',
+				borderRadius: 4,
+				height: 128,
+				width: 304,
+			}}>
+			<ArrowBackIosNewOutlinedIcon
+				className="leftArrow"
+				onClick={() => scrollToImage('prev')}
+			/>
+			<ArrowForwardIosIcon
+				className="rightArrow"
+				onClick={() => scrollToImage('next')}
+			/>
+			<div className="container-images">
+				<ul
+					className="ulSliderCard"
+					ref={listRef}>
+					{imgs.map((img, index) => {
+						return (
+							<li
+								className="liSliderCard"
+								key={index}>
+								<img
+									className="imgSliderCard"
+									src={img}
+									width={500}
+									height={280}
+								/>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="dots-container divSliderCard">
+				{imgs.map((_, idx) => (
+					<div
+						key={idx}
+						className={`dot-container-item ${
+							idx === currentIndex ? 'active' : ''
+						}`}
+						onClick={() => goToSlide(idx)}>
+						&#8226;
+					</div>
+				))}
+			</div>
+		</CardMedia>
+	);
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './assets/styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme/theme';
+import './assets/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
