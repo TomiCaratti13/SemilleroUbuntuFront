6e10b646d49614b001b08005379d0d244a33b256
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-03-06 21:55:18 -0300

    Add text in view, TODO link to categories

--- src/views/SectionMicroemprendimientos.jsx
+++ src/views/SectionMicroemprendimientos.jsx
@@ -1,5 +1,5 @@
 import { SectionHero } from '../components/SectionHero';
-import { Box } from '@mui/material';
+import { Container, Typography, Box } from '@mui/material';
 import microemprendimientos from '../utils/mocks/Microemprendimientos.json';
 import { MapMicroemprendimientos } from '../components/MapMicroemprendmientos';
 import { VectorGreen } from '../components/VectorGreen';
@@ -31,7 +31,45 @@ export const SectionMicroemprendmientos = () => {
         img={sectionPublicaiones.img}
       />
       <Box sx={{ position: 'relative', width: '100%', height: '100%' }}>
-        <VectorGreen />
+        <Container
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            gap: '4px',
+          }}>
+          <Typography
+            variant="h4"
+            sx={{ fontSize: '24px', lineHeight: '25px', fontWeight: 600 }}>
+            Categorías
+          </Typography>
+          <Typography
+            variant="h3"
+            sx={{
+              fontSize: '20px',
+              lineHeight: '30px',
+              fontWeight: 500,
+              color: 'azul.main',
+              textAlign: 'center',
+              textWrap: 'pretty',
+            }}>
+            Agroecología / Orgánicos / Alimentación saludable
+          </Typography>
+          <Typography
+            variant="h3"
+            sx={{
+              fontSize: '16px',
+              lineHeight: '25px',
+              fontWeight: 400,
+              textAlign: 'center',
+            }}>
+            Conectate con Microemprendimientos que respetan la tierra y
+            priorizan la salud, a través de prácticas agrícolas limpias y
+            alimentos nutritivos.
+          </Typography>
+        </Container>
+        <VectorGreen text={true} />
         <MapMicroemprendimientos microemprendimientos={microemprendimientos} />
       </Box>
     </Box>
