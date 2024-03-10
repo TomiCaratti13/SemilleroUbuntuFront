24f5e4f74543a74e2a83d125735722def055429d
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-03-09 21:23:12 -0300

    Actualizar componente MapMicroemprendimientos y  TODO SectionMicroemprendimientos

--- src/views/SectionMicroemprendimientos.jsx
+++ src/views/SectionMicroemprendimientos.jsx
@@ -1,10 +1,8 @@
 import { SectionHero } from '../components/SectionHero';
-import { Container, Typography, Box } from '@mui/material';
-import microemprendimientos from '../utils/mocks/Microemprendimientos.json';
-import { MapMicroemprendimientos } from '../components/MapMicroemprendmientos';
+import { Typography, Box } from '@mui/material';
 import { VectorGreen } from '../components/VectorGreen';
 import { MapCategorias } from '../components/MapCategorias';
-import  Categorias  from '../utils/mocks/Categorias';
+import Categorias from '../utils/mocks/Categorias';
 
 export const SectionMicroemprendmientos = () => {
   const sectionPublicaiones = {
@@ -24,7 +22,6 @@ export const SectionMicroemprendmientos = () => {
         gap: '30px',
         width: '100%',
         height: '100%',
-        paddingBottom: '30px',
       }}>
       <SectionHero
         category={sectionPublicaiones.category}
@@ -41,7 +38,7 @@ export const SectionMicroemprendmientos = () => {
           display: 'flex',
           flexWrap: 'wrap',
         }}>
-        <MapCategorias categorias={Categorias} />
+        <VectorGreen />
         <Typography
           variant="h4"
           sx={{
@@ -53,45 +50,9 @@ export const SectionMicroemprendmientos = () => {
           }}>
           Categorías
         </Typography>
-        <Container
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            gap: '10px',
-            width: '100%',
-          }}>
-          <Typography
-            variant="h3"
-            sx={{
-              fontSize: '20px',
-              lineHeight: '30px',
-              fontWeight: 500,
-              color: 'azul.main',
-              textAlign: 'center',
-              textWrap: 'pretty',
-            }}>
-            Agroecología/Orgánicos/ Alimentación saludable
-          </Typography>
-          <Typography
-            variant="h4"
-            sx={{
-              fontSize: '16px',
-              lineHeight: '25px',
-              fontWeight: 400,
-              textAlign: 'center',
-            }}>
-            Conectate con Microemprendimientos que respetan la tierra y
-            priorizan la salud, a través de prácticas agrícolas limpias y
-            alimentos nutritivos.
-          </Typography>
-          <VectorGreen text={true} />
-          <MapMicroemprendimientos
-            microemprendimientos={microemprendimientos}
-          />
-        </Container>
+        <MapCategorias categorias={Categorias} />
       </Box>
+      {/* Necesito que categories sea un estado y al clieckear en un categoria cambie el componente por un mapeo de microemprendimientos que coinciden con nuestro nombre */}
     </Box>
   );
 };
