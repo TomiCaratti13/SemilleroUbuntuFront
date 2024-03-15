ad22f6cb204015967a9cad7d2aa7f70032c6e051
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-03-15 20:07:43 -0300

    Min Merge

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,70 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
-import { store } from './redux/store.js';
+import { store } from './utils/redux/store.js';
+import './assets/styles/index.css';
+import { LandingPage } from './views/LandingPage.jsx';
+import Login from './views/Login.jsx';
+import { SectionPublicaciones } from './views/SectionPublicaciones.jsx';
+import { SectionMicroemprendmientos } from './views/SectionMicroemprendimientos.jsx';
+import { SectionFormularioContacto } from './views/SectionFormularioContacto.jsx';
+import ScrollToTop from './utils/services/scrollToTop.js';
+import { AdminDashboard } from './views/Admin/AdminDashboard.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route
+            path="/"
+            element={<App />}>
+            <Route
+              index
+              element={<LandingPage />}
+            />
+            <Route
+              path="login"
+              element={<Login />}
+            />
+            <Route
+              path="Admin/:token"
+              element={<AdminDashboard />}
+            />
+            <Route
+              path="microemprendimientos/:categoryUrl"
+              element={<SectionMicroemprendmientos />}
+            />
+            <Route
+              path="publicaciones"
+              element={<SectionPublicaciones />}
+            />
+            <Route
+              path="formularioContacto/:id"
+              element={<SectionFormularioContacto />}
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <Navigate
+                replace
+                to="/"
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
 import './assets/styles/index.css';
 import { LandingPage } from './views/LandingPage.jsx';
 import Login from './views/Login.jsx';
@@ -32,7 +95,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               element={<Login />}
             />
             <Route
-              path="Admin/:token"
+              path="Admin"
               element={<AdminDashboard />}
             />
             <Route
